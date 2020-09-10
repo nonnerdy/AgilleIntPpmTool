1b66bb70533ca1a05afaaa9bbacf3da15cc6efda
Biswajit <biswajiitjana@gmail.com>
2020-09-10 13:28:17 +0530

    first commit

---
+++ PPMToolFullStack/ppmtool/src/test/java/com/biswajit/fullstack/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biswajit.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
