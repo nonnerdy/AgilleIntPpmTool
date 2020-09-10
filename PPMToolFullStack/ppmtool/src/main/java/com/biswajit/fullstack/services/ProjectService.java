d323ad1406d46894ec788b23951fb43a6ff5650c
Biswajit <biswajiitjana@gmail.com>
2020-09-10 21:30:26 +0530

    Custom Exception Handling

--- PPMToolFullStack/ppmtool/src/main/java/com/biswajit/fullstack/services/ProjectService.java
+++ PPMToolFullStack/ppmtool/src/main/java/com/biswajit/fullstack/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.biswajit.fullstack.domain.Project;
+import com.biswajit.fullstack.exception.ProjectIdException;
 import com.biswajit.fullstack.repositories.ProjectRepository;
 
 @Service
@@ -11,9 +12,15 @@ public class ProjectService {
 
 	@Autowired
 	private ProjectRepository projectRepository;
-	
+
 	public Project savePrUpdateProject(Project project) {
-		//Logic 
-		return projectRepository.save(project);
+		// Logic
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		} catch (Exception ex) {
+			throw new ProjectIdException(
+					"Project Id '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+		}
 	}
 }
--- PPMToolFullStack/ppmtool/src/main/java/com/biswajit/fullstack/web/ProjectController.java
+++ PPMToolFullStack/ppmtool/src/main/java/com/biswajit/fullstack/web/ProjectController.java
@@ -1,8 +1,5 @@
 package com.biswajit.fullstack.web;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -10,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
