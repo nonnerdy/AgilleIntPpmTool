d422c92cfc603fd85784f494588d75d75a4f4153
Biswajit <biswajiitjana@gmail.com>
2020-09-12 19:57:50 +0530

    added Redux for state management , axio to connect to backend and showing data to frontend

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react'
-import Header from './Layout/Header';
-import CreateProjectButton from './project/CreateProjectButton';
-import ProjectItem from './project/ProjectItem';
+import React, { Component } from "react";
+import Header from "./Layout/Header";
+import CreateProjectButton from "./project/CreateProjectButton";
+import ProjectItem from "./project/ProjectItem";
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <div className="projects">
+  render() {
+    return (
+      <div className="projects">
         <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h1 className="display-4 text-center">Projects</h1>
-                    <br />
-                    <CreateProjectButton/>
-                    <br />
-                    <hr />
-                    <ProjectItem/>
-                </div>
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton />
+              <br />
+              <hr />
+              <ProjectItem />
             </div>
+          </div>
         </div>
-    </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 export default Dashboard;
